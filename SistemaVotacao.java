1f765c67fbe2ab093cfb12dd1c2842c03c7e3daa
Nicole Pimenta Dos Santos <nicole.11973394@aluno.mg.gov.br>
2026-08-24 13:43:55 -0300

    chore: criar estrutura inicial do programa

---
+++ SistemaVotacao.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class SistemaVotacao {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Sistema de votação iniciado.");
+        scanner.close();
+    }
+}
